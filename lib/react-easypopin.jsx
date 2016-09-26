1dfdbb5d7ac00adab9def299307126e012a4e4fd
D31K34N <deikean@gmail.com>
2016-09-26 20:45:10 +0200

    define transitionnal states and define new options for overlay and close buttons

--- lib/react-easypopin.jsx
+++ lib/react-easypopin.jsx
@@ -1,6 +1,17 @@
 import React, { PropTypes } from 'react';
 import './styles/react-easypopin.less';
-import * as Effects from './constants';
+import {
+    EFFECT_APPEAR_FROM_TOP_TO_BOTTOM,
+    EFFECT_APPEAR_FROM_BOTTOM_TO_TOP,
+    EFFECT_APPEAR_FROM_LEFT_TO_RIGHT,
+    EFFECT_APPEAR_FROM_RIGHT_TO_LEFT,
+    EFFECT_APPEAR_BY_HORIZONTAL_3D,
+    EFFECT_APPEAR_DEFAULT,
+    STATUS_OPENING,
+    STATUS_OPENED,
+    STATUS_CLOSING,
+    STATUS_CLOSED
+} from './constants';
 import classNames from 'classnames';
 /**
 * @author david pierre
@@ -30,60 +41,94 @@ export default class ReactEasyPopin extends React.Component {
     * defines state vars with props
     */
     componentDidMount () {
-        this.setState({'opened': this.props.opened});
-    }
-
-    /* let's expose constants for animation */
-    static Effects = Effects;
-
-    /* defines default properties */
-    static defaultProps = {
-        opened: false
+        this.setStatus(this.props.opened ? STATUS_OPENING : STATUS_CLOSED);
+        this.defineStatusChangingWithAnimationsEvents();
     }
 
     /* defines props types */
     static propTypes = {
         children: PropTypes.node,
         opened: PropTypes.bool,
+        closable: PropTypes.bool,
+        overlay: PropTypes.bool,
+        closeByOverlay: PropTypes.bool,
+        withCloseButton: PropTypes.bool,
         animation: PropTypes.string
     }
+
+    /* defines default properties */
+    static defaultProps = {
+        opened: false,
+        closable: true,
+        withCloseButton: true,
+        overlay: true,
+        closeByOverlay: true
+    }
+
+    /**
+      * defines the current status state
+      * @param status
+    **/
+    setStatus = (status) => this.setState({'status': status});
+
+    /**
+      * get the current status state
+     **/
+    getStatus = () => this.state && this.state.status;
+
+    /* let's expose constants for animation */
+    static Effects = {
+        'EFFECT_APPEAR_FROM_TOP_TO_BOTTOM': EFFECT_APPEAR_FROM_TOP_TO_BOTTOM,
+        'EFFECT_APPEAR_FROM_BOTTOM_TO_TOP': EFFECT_APPEAR_FROM_BOTTOM_TO_TOP,
+        'EFFECT_APPEAR_FROM_LEFT_TO_RIGHT': EFFECT_APPEAR_FROM_LEFT_TO_RIGHT,
+        'EFFECT_APPEAR_FROM_RIGHT_TO_LEFT': EFFECT_APPEAR_FROM_RIGHT_TO_LEFT,
+        'EFFECT_APPEAR_BY_HORIZONTAL_3D': EFFECT_APPEAR_BY_HORIZONTAL_3D,
+        'EFFECT_APPEAR_DEFAULT': EFFECT_APPEAR_DEFAULT
+    };
 
     /* returns the css class used for the animation
     * fallback is the prop, that could be used to
     * define customed animations css classes */
     getAnimation () {
-        return Effects[this.props.animation] || this.props.animation;
+        return this.props.animation;
+    }
+
+    defineStatusChangingWithAnimationsEvents () {
+        this.refs.handler.addEventListener('animationend', () => {
+            switch (this.getStatus()) {
+                case STATUS_OPENING:
+                    this.setStatus(STATUS_OPENED);
+                    break;
+                case STATUS_CLOSING:
+                    this.setStatus(STATUS_CLOSED);
+                    break;
+                default:
+                    break;
+            }
+        })
     }
 
     /**
-    * opens the popin defining its 'opened' state to true
-    * is defined to be called by parent component via "ref"
-    * or internally
-    */
+      * opens the popin defining its 'opened' state to true
+      * is defined to be called by parent component via "ref"
+      * or internally
+    **/
     open () {
-        this.setState({opened: true});
+        const status = this.getStatus();
+        (!status || status === STATUS_CLOSED)
+            && this.setStatus(STATUS_OPENING);
     }
 
     /**
-    * closes the popin defining its 'opened' state to false
-    * is defined to be called by parent component via "ref"
-    * or internally
-    */
+      * closes the popin defining its state to "closing"
+      * is defined to be called by parent component via "ref"
+      * or internally
+      * works only if the popin is defined as "closable"
+      */
     close () {
-        this.refs.handler.className = `react-easypopin__Handler react-easypopin__Closing ${this.getAnimation()}`;
-        this.refs.overlay.className = 'react-easypopin__Overlay react-easypopin__Overlay__Closing';
-
-        setTimeout(() => {
-            this.setState({opened: false});
-        }, 1000);
-    }
-
-    /**
-    * @return state.opened
-    */
-    isOpened () {
-        return this.state
-            && this.state.opened;
+        this.props.closable
+            && this.getStatus() === STATUS_OPENED
+            && this.setStatus(STATUS_CLOSING);
     }
 
     /**
@@ -91,17 +136,20 @@ export default class ReactEasyPopin extends React.Component {
     * to opened state
     */
     render () {
-        const opened = this.isOpened();
+        const status = this.getStatus();
+        const { closable, withCloseButton } = this.props;
         const classNameHandler = classNames({
             'react-easypopin__Handler': true,
-            'react-easypopin__Open' : opened,
-            'react-easypopin__Close' : !opened,
+            'react-easypopin__Open' : status === STATUS_OPENING || status === STATUS_OPENED,
+            'react-easypopin__Closed' : !status || status === STATUS_CLOSED,
+            'react-easypopin__Closing': status === STATUS_CLOSING,
             [`${this.getAnimation()}`]: true
         });
         const classNameOverlay = classNames({
             'react-easypopin__Overlay' : true,
-            'react-easypopin__Overlay__In' : opened,
-            'react-easypopin__Overlay__Out': !opened
+            'react-easypopin__Overlay__Open' : status === STATUS_OPENING || status === STATUS_OPENED,
+            'react-easypopin__Overlay__Closing': status === STATUS_CLOSING,
+            'react-easypopin__Overlay__Closed': !status || status === STATUS_CLOSED
         });
         return (
             <div className="react-easypopin">
@@ -117,12 +165,14 @@ export default class ReactEasyPopin extends React.Component {
                     <div
                         className="react-easypopin__Handler__Content"
                     >
-                      <a
-                          onClick={this.close}
-                          className="react-easypopin__Handler__Close"
-                      >
-                          fermer
-                      </a>
+                      {closable && withCloseButton && (
+                          <a
+                              onClick={this.close}
+                              className="react-easypopin__Handler__Close"
+                          >
+                              fermer
+                          </a>
+                      )}
                       {this.props.children}
                     </div>
                 </div>
